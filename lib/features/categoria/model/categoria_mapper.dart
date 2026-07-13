81b9358d55e73a285f98fde0e20fd53b1bb332fd
Sampaio-Luan <luansampaio.dev@gmail.com>
2026-07-13 03:44:42 -0300

    feat: Adicionei modelo Categoria

---
+++ lib/features/categoria/model/categoria_mapper.dart
@@ -0,0 +1,55 @@
+import '../../../core/constants/enums/cores.dart';
+import '../../../core/contracts/contrato_mapper.dart';
+import '../../../core/database/schema/tb_categoria.dart';
+import '../../../core/utils/data_utils.dart';
+
+import 'categoria_model.dart';
+
+class CategoriaMapper implements ContratoMapper<Categoria> {
+  @override
+  Categoria daNuvem(Map<String, dynamic> map) {
+    return Categoria.padrao();
+  }
+
+  @override
+  Categoria doBd(Map<String, dynamic> map) {
+    return Categoria(
+      id: map[TbCategoria.colunaId],
+      titulo: map[TbCategoria.colunaTitulo],
+      cor: Cor.obterCor(
+        cor: Cor.obterPorRotulo(rotulo: map[TbCategoria.colunaCor]),
+      ),
+      ordem: map[TbCategoria.colunaOrdem],
+      descricao: map[TbCategoria.colunaDescricao],
+      dtCriacao: DataUtils.strParaData(
+        strData: map[TbCategoria.colunaDataCriacao],
+      ),
+      dtEdicao: DataUtils.strParaData(
+        strData: map[TbCategoria.colunaDataAlteracao],
+      ),
+      estaExcluido: map[TbCategoria.colunaEstaExcluido] == 1 ? true : false,
+    );
+  }
+
+  @override
+  Map<String, dynamic> paraBd(Categoria objeto) {
+    return {
+      if (objeto.id != null) TbCategoria.colunaId: objeto.id,
+      TbCategoria.colunaTitulo: objeto.titulo,
+      TbCategoria.colunaCor: Cor.obterPorColor(color: objeto.cor).name,
+      TbCategoria.colunaOrdem: objeto.ordem,
+      if (objeto.descricao != null)
+        TbCategoria.colunaDescricao: objeto.descricao,
+      if (objeto.dtCriacao != null)
+        TbCategoria.colunaDataCriacao: objeto.dtCriacao!.toIso8601String(),
+      if (objeto.dtEdicao != null)
+        TbCategoria.colunaDataAlteracao: objeto.dtEdicao!.toIso8601String(),
+      TbCategoria.colunaEstaExcluido: objeto.estaExcluido ? 1 : 0,
+    };
+  }
+
+  @override
+  Map<String, dynamic> paraNuvem(Categoria objeto) {
+    return {};
+  }
+}
---
+++ lib/features/categoria/model/categoria_model.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/constants/enums/cores.dart';
+import '../../../core/utils/data_utils.dart';
+
+class Categoria {
+  int? id;
+  String titulo;
+  Color cor;
+  int ordem;
+  String? descricao;
+  DateTime? dtCriacao;
+  DateTime? dtEdicao;
+  bool estaExcluido;
+
+  // #region ======================== Construtores =========================
+
+  Categoria({
+    this.id,
+    required this.titulo,
+    required this.cor,
+    required this.ordem,
+    this.descricao,
+    this.dtCriacao,
+    this.dtEdicao,
+    this.estaExcluido = false,
+  });
+
+  static Categoria padrao() {
+    return Categoria(
+      id: null,
+      titulo: '',
+      cor: Cor.obterListaCores()[0],
+      ordem: 0,
+      descricao: null,
+      dtCriacao: null,
+      dtEdicao: null,
+      estaExcluido: false,
+    );
+  }
+
+  // #endregion ===================== End Construtores ====================
+
+  // #region ======================== Setters =============================
+  String setTitulo(String titulo) => this.titulo = titulo;
+  Color setCor(Cor cor) => this.cor = Cor.obterCor(cor: cor);
+  int setOrdem(int ordem) => this.ordem = ordem;
+  void setDescricao(String descricao) {
+    descricao.isEmpty ? this.descricao = null : this.descricao = descricao;
+  }
+
+  // #endregion ===================== End Setters ==========================
+
+  @override
+  String toString() {
+    return 'Categoria{\nid: $id, \ntitulo: $titulo, \ncor: $cor, \nordem: $ordem, \ndescricao: $descricao, \ndtCriacao: ${DataUtils.dataParaStr(data: dtCriacao!)}, \ndtEdicao: ${DataUtils.dataParaStr(data: dtEdicao!)}, \nestaExcluido: $estaExcluido \n}';
+  }
+}
