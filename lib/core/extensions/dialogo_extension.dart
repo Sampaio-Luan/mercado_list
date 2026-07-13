9ced6165504e659119c94533c2022cde8d7d3d0f
Sampaio-Luan <luansampaio.dev@gmail.com>
2026-07-13 03:05:51 -0300

    feat: Adicionei extensões compartilhadas

---
+++ lib/core/extensions/dialogo_extension.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../data/constants/enum/tipo_dialogo.dart';
+import '../service/dialogo.service.dart';
+
+extension DialogoExtension on BuildContext {
+  Future<void> sucesso({required String mensagem, String titulo = 'Sucesso'}) {
+    return DialogoService.mostrar(
+      context: this,
+      tipo: TipoDialogo.sucesso,
+      titulo: titulo,
+      mensagem: mensagem,
+    );
+  }
+
+  Future<void> erro({required String mensagem, String titulo = 'Erro'}) {
+    return DialogoService.mostrar(
+      context: this,
+      tipo: TipoDialogo.erro,
+      titulo: titulo,
+      mensagem: mensagem,
+    );
+  }
+
+  Future<void> informacao({
+    required String mensagem,
+    String titulo = 'Informação',
+  }) {
+    return DialogoService.mostrar(
+      context: this,
+      tipo: TipoDialogo.informacao,
+      titulo: titulo,
+      mensagem: mensagem,
+    );
+  }
+
+  Future<ResultadoDialogo?> confirmar({
+    required String titulo,
+    required String mensagem,
+    String textoConfirmar = 'Confirmar',
+    String textoCancelar = 'Cancelar',
+  }) {
+    return DialogoService.mostrar(
+      context: this,
+      tipo: TipoDialogo.aviso,
+      titulo: titulo,
+      mensagem: mensagem,
+      textoConfirmar: textoConfirmar,
+      textoCancelar: textoCancelar,
+      exibirCancelar: true,
+    );
+  }
+}
---
+++ lib/core/extensions/snackbar_extension.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../data/constants/enum/tipo_snackbar.dart';
+import '../service/snackbar.service.dart';
+
+
+extension SnackbarExtension on BuildContext {
+  void mostrarSucesso(String mensagem) {
+    SnackbarService.mostrar(
+      context: this,
+      mensagem: mensagem,
+      tipo: TipoSnackbar.sucesso,
+    );
+  }
+
+  void mostrarErro(String mensagem) {
+    SnackbarService.mostrar(
+      context: this,
+      mensagem: mensagem,
+      tipo: TipoSnackbar.erro,
+    );
+  }
+
+  void mostrarAviso(String mensagem) {
+    SnackbarService.mostrar(
+      context: this,
+      mensagem: mensagem,
+      tipo: TipoSnackbar.aviso,
+    );
+  }
+
+  void mostrarInfo(String mensagem) {
+    SnackbarService.mostrar(
+      context: this,
+      mensagem: mensagem,
+      tipo: TipoSnackbar.informacao,
+    );
+  }
+}
